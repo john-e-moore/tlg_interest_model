0e78cf24ca9f549c9ca065f400877206fc04acfc
john-e-moore <jmoore87jr@gmail.com>
2024-02-14 14:54:42 -0500

    setting up repo; new files.

---
+++ README.md
@@ -0,0 +1,3 @@
+# US Government Interest Payment Simulator
+
+The purpose of this project is to simulate the US Government's debt burden in years to come given a set of assumptions. It accounts for debts maturing and being reissued at different interest rates.
---
+++ src/main.py
@@ -0,0 +1,12 @@
+def main():
+    """
+    Objective: Calculate US Gov't debt burden thru 2030.
+
+    Assumptions:
+    * 5% interest rate when debt gets reissued.
+    * All debts get reissued immediately when the mature.
+    """
+    pass
+
+if __name__ == "__main__":
+    main()
