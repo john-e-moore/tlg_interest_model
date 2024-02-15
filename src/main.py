465c086165df7a24de81a7ebe3d98ad07d82791d
john-e-moore <jmoore87jr@gmail.com>
2024-02-15 06:05:43 -0500

    figured-ish out how to do the calculation part

--- src/main.py
+++ src/main.py
@@ -52,31 +52,73 @@ def main():
     usecols = [
         'Security Type Description', # Marketable or Non-Marketable
         'Security Class 1 Description', # Type of security
-        'Security Class 2 Description', 
+        'Security Class 2 Description', # CUSIP ID number (unique for each security)
         'Interest Rate',
         'Yield',
         'Issue Date',
         'Maturity Date',
-        'Issued Amount (in Millions)',
-        'Outstanding Amount (in Millions)',
-        'Fiscal Year',
-        'Fiscal Quarter Number',
-        'Calendar Year',
-        'Calendar Quarter Number',
-        'Calendar Month Number',
-        'Calendar Day Number'
+        'Issued Amount (in Millions)', # Read as non-numeric due to *
+        'Outstanding Amount (in Millions)', # Read as non-numeric due to *
     ]
     raw_data_path = config['raw_data_path']
 
     df = pd.read_csv(raw_data_path, usecols=usecols)
     print(df.head())
-    print(df.columns)
+    print(df.dtypes)
     print(df.shape)
 
     # Filter data
-    class_1_descriptions_to_keep = ('Notes', 'Bonds')
+    # Only keep non-TIPS, non-FSN marketable securities
+    class_1_descriptions_to_keep = ('Notes', 'Bonds', 'Bills Maturity Value')
     df = df[df['Security Class 1 Description'].isin(class_1_descriptions_to_keep)]
+    print("Non-marketable securites, TIPS, and FSN's removed.")
     print(df.shape)
+    # Drop duplicates
+    # Raw data seems to be append-only, so same security can have multiple rows
+    # But, securities can be re-issued (added onto) at a later date
+    subset = ['Security Class 2 Description', 'Issue Date']
+    df.drop_duplicates(subset=subset, keep='first', inplace=True)
+    print("Duplicates dropped.")
+    print(df.shape)
+    # Remove totals
+    df = df[~df['Security Class 2 Description'].str.contains('Total')]
+    print("Totals records removed.")
+    print(df.shape)
+
+    # Change columns to numeric
+    df['Issued Amount (in Millions)'] = pd.to_numeric(df['Issued Amount (in Millions)'])
+    df['Outstanding Amount (in Millions)'] = pd.to_numeric(df['Outstanding Amount (in Millions)'])
+
+    df.to_csv('sample.csv')
+    # NOTE: Calculation needs to begin here; can't collapse data because
+    # The security can be added on to. 
+    # Create a function that takes a single security, iterates, and outputs
+    # a dict with:
+    """
+    {
+        id: 938848,
+        security_type: bond,
+        initial_issue_date: 1/1/2020,
+        maturity_date: 1/1/2024,
+        interest_paid:
+        {
+            2020: 80,
+            2021: 100,
+            2022: 100,
+            2023: 100,
+            2024: 20
+        }
+    }
+    """
+    # then create a dataframe from this dict with the necessary columns for each year
+
+
+
+
+
+
+
+
 
     # Collapse data
     # Group by Security Class 2 Description
@@ -84,13 +126,28 @@ def main():
     # Average Interest Rate
     grouped_df = df.groupby('Security Class 2 Description').agg({
         'Issued Amount (in Millions)': 'sum',
-        'Interest Rate': 'mean'
+        'Interest Rate': 'mean',
+        'Yield': 'mean'
     })
-    print(grouped_df.head())
+    grouped_df = df.groupby('Security Class 2 Description').agg({
+        'Security Class 2 Description': 'count',
+        'Issued Amount (in Millions)': 'sum',
+        'Interest Rate': 'mean',
+        'Yield': 'mean'
+    })
+    print(grouped_df.head(20))
     print(grouped_df.columns)
     print(grouped_df.shape)
 
+    # Examine why there are still duplicate IDs
+    test_df = df[df['Security Class 2 Description'] == '912757VP3']
+    print(test_df)
+    # Maturity dates are all the same, but issue date is not
+    # 
+
     # Left join 
+
+    # Add issue date calendar year
 
 if __name__ == "__main__":
     main()
