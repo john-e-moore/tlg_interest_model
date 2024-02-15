b939d96701bc63fc4fdd77a7cf3580a9c999b718
john-e-moore <jmoore87jr@gmail.com>
2024-02-15 15:45:24 -0500

    calculating interest payment for a single row working. just need to figure out how to recombine.

--- src/main.py
+++ src/main.py
@@ -7,6 +7,92 @@ def load_config(config_path):
     with open(config_path, 'r') as file:
         return yaml.safe_load(file)
 
+def calculate_fraction_of_year_remaining(_month: int, _day: int):
+    """Use for issuing year."""
+    days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    total_days_in_year = 365
+
+    # Calculate total number of days that have passed in the year up to the given date.
+    days_passed = sum(days_in_month[:_month - 1]) + _day
+
+    return (total_days_in_year - days_passed) / total_days_in_year
+
+def calculate_fraction_of_year_elapsed(_month: int, _day: int):
+    """Use for maturing year."""
+    days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    total_days_in_year = 365
+
+    # Calculate total number of days that have passed in the year up to the given date.
+    days_passed = sum(days_in_month[:_month - 1]) + _day
+
+    return days_passed / total_days_in_year
+
+def process_raw_row(row): 
+    """
+    Iterates through the years a security was live and returns a dictionary
+    containing the interest payment for each year.
+
+    Parameters:
+    row: A single row of a Pandas DataFrame
+
+    Returns:
+    {
+        id: 938848,
+        security_type: bond,
+        initial_issue_date: 1/1/2020,
+        maturity_date: 1/1/2024,
+        interest_paid:
+        {
+            2020: 80,
+            2021: 100,
+            2022: 100,
+            2023: 100,
+            2024: 20
+        }
+    }
+    """
+    # Create a dict with keys being years between issue and maturity, inclusive.
+    year_issued = row['year_issued']
+    year_matured = row['year_matured']
+    interest_payments = {x: 0 for x in range(year_issued, year_matured+1)}
+
+    # Calculate year fractions for issue and mature years. 
+    month_issued = row['month_issued']
+    day_issued = row['day_issued']
+    month_matured = row['month_matured']
+    day_matured = row['day_matured']
+    fraction_of_year_remaining_after_issue = calculate_fraction_of_year_remaining(month_issued, day_issued)
+    fraction_of_year_elapsed_before_maturity = calculate_fraction_of_year_elapsed(month_matured, day_matured)
+
+    # Initialize interest rate variable.
+    # Bonds and Notes use interest rate. Bills don't have one; use yield.
+    if not np.isnan(row['Interest Rate']):
+        interest_rate = row['Interest Rate'] / 100
+
+    else:
+        interest_rate = row['Yield'] / 100
+
+    # Calculate interest payments and update dict. 
+    issue_amount = row['Issued Amount (in Millions)']
+    for i,_year in enumerate(interest_payments.keys()):
+        if i == 0: # Year issued
+            interest_payment = fraction_of_year_remaining_after_issue * issue_amount * interest_rate
+        elif i == len(interest_payments.keys()) - 1: # Year matured
+            interest_payment = fraction_of_year_elapsed_before_maturity * issue_amount * interest_rate
+        else: # Full year
+            interest_payment = issue_amount * interest_rate
+        # Update dictionary
+        interest_payments.update({_year: interest_payment})
+    
+    return {
+        'id': row['Security Class 2 Description'],
+        'security_type': row['Security Class 1 Description'],
+        'issue_date': row['Issue Date'],
+        'maturity_date': row['Maturity Date'],
+        'interest_paid': interest_payments
+    }
+
+
 def main():
     """
     Objective: Calculate US Gov't debt burden thru 2030.
@@ -89,7 +175,27 @@ def main():
     df['Issued Amount (in Millions)'] = pd.to_numeric(df['Issued Amount (in Millions)'])
     df['Outstanding Amount (in Millions)'] = pd.to_numeric(df['Outstanding Amount (in Millions)'])
 
-    df.to_csv('sample.csv')
+    # Change columns to datetime after dropping nulls
+    df.dropna(subset=['Issue Date', 'Maturity Date'], inplace=True) # (1)
+    df['Issue Date'] = pd.to_datetime(df['Issue Date'])
+    df['Maturity Date'] = pd.to_datetime(df['Maturity Date'])
+
+    print(df[['Issue Date', 'Maturity Date']].head())
+    print(df.dtypes)
+
+    # Add year, month, day columns for interest payment calculation
+    df['year_issued'] = df['Issue Date'].dt.year
+    df['month_issued'] = df['Issue Date'].dt.month
+    df['day_issued'] = df['Issue Date'].dt.day
+    df['year_matured'] = df['Maturity Date'].dt.year
+    df['month_matured'] = df['Maturity Date'].dt.month
+    df['day_matured'] = df['Maturity Date'].dt.day
+
+    # Process an example row
+    result = process_raw_row(df.iloc[0,:])
+    print(result)
+
+    df.to_csv('sample.csv', index=False)
     # NOTE: Calculation needs to begin here; can't collapse data because
     # The security can be added on to. 
     # Create a function that takes a single security, iterates, and outputs
